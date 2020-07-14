bde78f55f3e823aad98f441d612f79953f5ec302
Ben <20133902+thisbenrogers@users.noreply.github.com>
2020-07-14 16:22:14 -0500

    Reduces int() calls

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -32,9 +32,9 @@ print(y)
 # Use a list comprehension to create a list containing only the _even_ elements
 # the user entered into list x.
 
-x = input("Enter comma-separated numbers: ").split(',')
+x = map(int, input("Enter comma-separated numbers: ").split(','))
 
 # What do you need between the square brackets to make it work?
-y = [int(z) for z in x if int(z) % 2 == 0]
+y = [z for z in x if not z % 2]
 
 print(y)
